b6ae5cbfd218802efb327b5acc885527fb42c886
Manuel <manuelbuldr@gmail.com>
2021-07-02 17:00:57 +0200

    Removed Registration - updated the routes/auth.php file

--- app/Http/Controllers/BeaconController.php
+++ app/Http/Controllers/BeaconController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Models\Beacon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
-use Illuminate\Support\Facades\Auth;
 
 class BeaconController extends Controller
 {
